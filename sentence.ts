c2bc269db55d57dc63c9e15dd4e95582e7d4aba1
muhammad-usama-1271999 <160097159+muhammad-usama-1271999@users.noreply.github.com>
2024-04-17 09:50:32 +0500

    this file check the length of sentence

---
+++ sentence.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let user : {
+    sentence : string
+} = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : "write a sentence and check the length of sentence"
+    }
+]);
+
+let sentence = user.sentence.trim().split(" ")
+
+console.log(sentence);
+
+let length = sentence.length
+
+console.log(`the length of your sentence is: ${length}`);
